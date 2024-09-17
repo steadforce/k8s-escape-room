88ad9eb602bd001b06bb5c9654a5d3a1a0b18221
Markus Dippold <markus.dippold@steadforce.com>
2024-09-17 13:39:31 +0200

    Install add-ons if a add-on-directory is given as parameter to init.sh

--- init.sh
+++ init.sh
@@ -6,13 +6,27 @@ DIR="$(cd "$(dirname "$0")" >/dev/null 2>&1; pwd -P)"
 
 INIT_DIR="$DIR/init.d"
 
-for f in $(ls "${INIT_DIR}"); do
-  CURDIR="${INIT_DIR}/${f}"
-  echo -e "\e[1m\e[92mProcessing ${f}...\e[0m"
-  "${CURDIR}"/install.sh
+# install all escape room modules
+#for f in $(ls "${INIT_DIR}"); do
+#  CURDIR="${INIT_DIR}/${f}"
+#  echo -e "\e[1m\e[92mProcessing ${f}...\e[0m"
+#  "${CURDIR}"/install.sh
+#done
+
+# install all modules of add-ons
+# directories of addons are given as parameter to init script
+for addOnPath in "$@"; do
+  INIT_DIR="$DIR/${addOnPath}/init.d"
+  echo ""
+  echo -e "\e[1m\e[4m\e[33mInstalling Add-on "$(basename "$addOnPath")"\e[0m"
+  for f in $(ls "${INIT_DIR}"); do
+    CURDIR="${INIT_DIR}/${f}"
+    echo -e "\e[1m\e[92m  Processing ${f}...\e[0m"
+    "${CURDIR}"/install.sh
+  done
 done
 
-
+# export kubeconfig variable
 if [ -f .kubeconfig ]; then
     export KUBECONFIG="$DIR/.kubeconfig"
 fi
