3804b35b293d8e5297d3f0133f2afa8ca1111b01
Markus Dippold <116820445+MarDipp@users.noreply.github.com>
2024-09-19 12:48:27 +0200

    Install add ons (#56)
    * Install add-ons if a add-on-directory is given as parameter to init.sh
    
    * Initialize cluster before installing the add-on
    
    * Formatting

--- init.sh
+++ init.sh
@@ -6,13 +6,28 @@ DIR="$(cd "$(dirname "$0")" >/dev/null 2>&1; pwd -P)"
 
 INIT_DIR="$DIR/init.d"
 
+# install all escape room modules
+echo -e "\e[1m\e[4m\e[33mInstalling Kubernetes Escape Room\e[0m"
 for f in $(ls "${INIT_DIR}"); do
   CURDIR="${INIT_DIR}/${f}"
   echo -e "\e[1m\e[92mProcessing ${f}...\e[0m"
   "${CURDIR}"/install.sh
 done
 
+# install all modules of add-ons
+# directories of addons are given as parameter to init script
+for addOnPath in "$@"; do
+  INIT_DIR="$DIR/${addOnPath}/init.d"
+  echo ""
+  echo -e "\e[1m\e[4m\e[33mInstalling Add-on "$(basename "$addOnPath")"\e[0m"
+  for f in $(ls "${INIT_DIR}"); do
+    CURDIR="${INIT_DIR}/${f}"
+    echo -e "\e[1m\e[92mProcessing ${f}...\e[0m"
+    "${CURDIR}"/install.sh
+  done
+done
 
+# export kubeconfig variable
 if [ -f .kubeconfig ]; then
     export KUBECONFIG="$DIR/.kubeconfig"
 fi
