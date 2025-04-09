08e6453d6643463ba3b1ae237a751364d420f194
David Peters <david.peters@steadforce.com>
2025-04-09 06:35:41 +0000

    add pr review suggestions

--- init.d/02.setup-resources/install.sh
+++ init.d/02.setup-resources/install.sh
@@ -4,10 +4,8 @@ set -e
 
 DIR="$(cd "$(dirname "$0")" >/dev/null 2>&1; pwd -P)"
 
-echo "Apply all manifests ..."
-for file in $(ls ${DIR}/manifests/*.yaml | grep -v 'kustomization.yaml'); do
-  kubectl apply -f $file
-done
+echo "Apply all manifests except kustomization ..."
+find "$DIR/manifests/" -name '*.yaml' ! -name 'kustomization.yaml' -exec kubectl apply -f {} \;
 
 echo "Apply kustomize manifests ..."
 kustomize build ${DIR}/manifests | kubectl apply -f -
