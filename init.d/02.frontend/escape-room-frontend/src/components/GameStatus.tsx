a2f789b4cb2e2a70a0bc7b739703ac0f515215a7
David Peters <david.peters@steadforce.com>
2025-06-04 10:14:12 +0200

    GameStatus component

--- init.d/02.frontend/escape-room-frontend/src/components/GameStatus.tsx
+++ init.d/02.frontend/escape-room-frontend/src/components/GameStatus.tsx
@@ -1,11 +1,24 @@
-import { useContext } from "react"
+import { useContext, useEffect, useState } from "react"
 import { GameContext } from "../main"
+import './GameStatus.css'
 
 function GameStatus() {
     const startTime = useContext(GameContext)
+    const [timer, setTimer] = useState("")
+
+    useEffect(() => {
+        setInterval(() => {
+            const elapsed = new Date(new Date().getTime() - startTime.getTime()).toISOString().substring(11, 19)
+            setTimer(elapsed)
+        }, 1000)
+
+    }, [])
+
     return (
         <>
-            { new Date(new Date().getTime() - startTime.getTime()).toISOString().substring(11, 19) }
+            <div className="timer">
+                { timer }
+            </div>
         </>
     )
 }
