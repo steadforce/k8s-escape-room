ba39deea9fc8c9ef106a1c513df7bcb10c0d0b9a
David Peters <david.peters@steadforce.com>
2025-06-04 14:58:24 +0200

    status display and cat riddle red circle

--- init.d/02.frontend/escape-room-frontend/src/components/GameStatus.tsx
+++ init.d/02.frontend/escape-room-frontend/src/components/GameStatus.tsx
@@ -5,19 +5,22 @@ import './GameStatus.css'
 function GameStatus() {
     const startTime = useContext(GameContext)
     const [timer, setTimer] = useState("")
+    const [progress, setProgress] = useState("✅ ✅ ✅ ❌")
 
     useEffect(() => {
         setInterval(() => {
             const elapsed = new Date(new Date().getTime() - startTime.getTime()).toISOString().substring(11, 19)
             setTimer(elapsed)
         }, 1000)
-
     }, [])
 
     return (
         <>
             <div className="timer">
                 { timer }
+            </div>
+            <div className="progress">
+                { progress }
             </div>
         </>
     )
