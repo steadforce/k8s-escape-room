c88c18fe4eafd70c7f3d66c733674329da22cfa7
David Peters <david.peters@steadforce.com>
2025-06-05 15:22:02 +0200

    check gamestate for display

--- init.d/02.frontend/escape-room-frontend/src/components/GameStatus.tsx
+++ init.d/02.frontend/escape-room-frontend/src/components/GameStatus.tsx
@@ -28,8 +28,37 @@ function GameStatus() {
     })
     const [progress, setProgress] = useState("✅ ✅ ✅ ❌")
 
+    const checkState = async () => {
+        const catState = await fetch("/cat").then(r => {return r.ok})
+        const orbState = await fetch("/orb").then(r => {return r.ok})
+        const tomeState = await fetch("/tome").then(r => {return r.ok})
+        const photoFrameState = await fetch("/photoframe/photo0.png").then(r => {return r.ok})
+        setGameState({
+            running: true,
+            solved: false,
+            name: "",
+            puzzles: {
+                cat: {
+                    solved: catState,
+                    replicas: 0,
+                },
+                orb: {
+                    solved: orbState,
+                },
+                photo: {
+                    solved: photoFrameState,
+                    url: "",
+                },
+                tome: {
+                    solved: tomeState,
+                }
+            },
+        })
+    }
+
     useEffect(() => {
         setInterval(() => {
+            checkState()
             const elapsed = new Date(new Date().getTime() - startTime.getTime()).toISOString().substring(11, 19)
             setTimer(elapsed)
         }, 1000)
