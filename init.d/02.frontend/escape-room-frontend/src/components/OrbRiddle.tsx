97b6351a433b159378b0a1c0a2f187cc6549e2f3
David Peters <david.peters@steadforce.com>
2025-06-05 13:52:00 +0200

    orb riddle check state

--- init.d/02.frontend/escape-room-frontend/src/components/OrbRiddle.tsx
+++ init.d/02.frontend/escape-room-frontend/src/components/OrbRiddle.tsx
@@ -1,15 +1,31 @@
+import { useEffect, useState } from "react"
+
 function OrbRiddle() {
+    const [solved, setSolved] = useState(false)
+
+    useEffect(() => {
+        const checkCatState = async () => {
+            await fetch("/orb").then(r => {
+                setSolved(r.ok)
+            })
+        }
+
+        setInterval(async () => {
+            await checkCatState()
+        }, 2000)
+    }, [])
     return (
         <>
-            <div className="orb">
+            <div className="orb" hidden={!solved}>
                 <img id="orb" src="/orb.png" />
-                <img id="orbWisdom" className="orbWisdom" />
+                <img id="orbWisdom" className="orbWisdom" src="/orb" />
             </div>
             <img
                 id="orbHint"
                 className="orbHint"
                 src="/redcircle.png"
                 title="The magic orb does not work!"
+                hidden={solved}
             />
         </>
     )
